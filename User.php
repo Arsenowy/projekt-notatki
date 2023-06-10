124e62e68a7593f2cbcfd3dd22e85812f837d06d
mikolaj-krawczyk-droptica <mikolaj.krawczyk@droptica.com>
2023-06-10 20:59:42 +0200

    PO-004 add user class

---
+++ User.php
@@ -0,0 +1,38 @@
+
+<?php
+
+class User {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    public function register($login, $email, $password) {
+        // Hash the password before storing it
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        $stmt = $this->conn->prepare("INSERT INTO users (login, email, password) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $login, $email, $hashedPassword);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function login($login, $password) {
+        $stmt = $this->conn->prepare("SELECT password FROM users WHERE login = ?");
+        $stmt->bind_param("s", $login);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+
+        if ($row && password_verify($password, $row['password'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
